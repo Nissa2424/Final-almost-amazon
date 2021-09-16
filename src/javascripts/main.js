49fc06dd3cc7061de5051e7916207c5de341c38c
Charnissa McKinney <86807857+Nissa2424@users.noreply.github.com>
2021-09-15 21:41:22 -0500

    Initial commit

---
+++ src/javascripts/main.js
@@ -0,0 +1,8 @@
+import checkLoginStatus from './helpers/router';
+
+import 'bootstrap'; // import bootstrap elements and js
+import '../styles/main.scss';
+
+(function init() {
+  checkLoginStatus();
+}());
